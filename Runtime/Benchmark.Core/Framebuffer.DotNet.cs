26499f0385578d5f6beb72ec30bfd46e409e3f3a
cNoNim <my@cnonim.name>
2024-07-30 11:21:34 +0300

    Fix Framebuffer in DotNet

--- Runtime/Benchmark.Core/Framebuffer.DotNet.cs
+++ Runtime/Benchmark.Core/Framebuffer.DotNet.cs
@@ -1,53 +1,73 @@
 #if !UNITY_2020_3_OR_NEWER
 using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using Benchmark.Core.Components;
 using Benchmark.Core.Hash;
 
 namespace Benchmark.Core
 {
-	public struct Framebuffer : IDisposable
+
+public readonly struct Framebuffer : IDisposable
+{
+	private readonly uint[]         _buffer;
+	private readonly List<DrawCall> _draws;
+	private readonly int            _width;
+	private readonly int            _height;
+
+	public Framebuffer(int width, int height, int capacity = 0)
 	{
-		private readonly uint[] _buffer;
-		private readonly DrawCall[] _draws;
-		private readonly int _width;
-		private readonly int _height;
-		private int _drawCount;
-
-		public Framebuffer(int width, int height, int capacity = 0)
-		{
-			_width = width;
-			_height = height;
-			_buffer = new uint[width * height];
-			_draws = new DrawCall[capacity];
-			_drawCount = 0;
-		}
-
-		public ReadOnlySpan<uint> Buffer => _buffer;
-
-		public ReadOnlySpan<DrawCall> Draws => new(_draws, 0, _drawCount);
-
-		public uint HashCode =>
-			StableHash32.Hash(0, _buffer);
-
-		public void Clear()
-		{
-			_buffer.AsSpan().Clear();
-		}
-
-		public void Draw(int tick, uint id, int x, int y, SpriteMask c)
-		{
-			if (_drawCount < _draws.Length)
-				_draws[_drawCount++] = new DrawCall(tick, id, x, y, c);
-			if (x < 0 || x >= _width ||
-			    y < 0 || y >= _height)
-				return;
-			var index = x + y * _width;
-			_buffer[index] |= (uint)c;
-		}
-
-		public void Dispose()
-		{
-		}
+		_width  = width;
+		_height = height;
+		_buffer = new uint[width * height];
+		_draws  = new List<DrawCall>(capacity);
 	}
+
+	public ReadOnlySpan<uint> Buffer
+	{
+		get => _buffer;
+	}
+
+	public ReadOnlySpan<DrawCall> Draws
+	{
+		get => CollectionsMarshal.AsSpan(_draws);
+	}
+
+	public uint HashCode
+	{
+		get => StableHash32.Hash(0, _buffer);
+	}
+
+	public void Clear() =>
+		_buffer.AsSpan()
+			   .Clear();
+
+	public void Draw(
+		int tick,
+		uint id,
+		int x,
+		int y,
+		SpriteMask c)
+	{
+		if (_draws.Count < _draws.Capacity)
+			_draws.Add(
+				new DrawCall(
+					tick,
+					id,
+					x,
+					y,
+					c));
+		if (x < 0
+		 || x >= _width
+		 || y < 0
+		 || y >= _height)
+			return;
+		var index = x + y * _width;
+		_buffer[index] |= (uint) c;
+	}
+
+	public void Dispose() {}
+}
+
 }
 #endif
